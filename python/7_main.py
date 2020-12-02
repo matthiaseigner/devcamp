4e581071e333e3acca96a8bd0423ee75c344b49e
florianmatzka <florian@matzka.biz>
2020-12-02 10:59:28 +0100

    printed ects.series and sst.series of courses.df; printed sum of ects and sst as well as average of ects per class

--- python/7_main.py
+++ python/7_main.py
@@ -4,14 +4,34 @@ import numpy as np
 def sum_of_ects(courses_df):
     sum_ects = courses_df['ECTS'].sum()
     return sum_ects
+
+def sum_of_sst(courses_df):
+    sum_sst = courses_df['SSt.'].sum()
+    return sum_sst
     
 if __name__ == "__main__": 
     ects_df = pd.read_csv('ects.csv')
 
     # print sum of ects
     sum_ects = sum_of_ects(ects_df)
-    print(f'You have {sum_ects:.0f} ects in total')
+    print(f'You have {sum_ects:.0f} ECTS in total.')
 
-    # pring ects of semesters
+    # print ects of semesters
+    # fehlt
+
+    # print sum of SSt. 
+    sum_sst = sum_of_sst(ects_df)
+    print(f'You have {sum_sst:.0f} semester hours in total.')
+
+    # print spacer
+    print('-')
+
+    # print average of ects
+    number_of_rows = ects_df.shape[0]-1
+    print(f'The number of classes, in which {sum_ects:.0f} ects have been acquired, is {number_of_rows:.0f}.')
+    average_ects = sum_ects/number_of_rows
+    print(f'On average you have acquired {average_ects:.2f} ects per class.')
+    
+
 
     
